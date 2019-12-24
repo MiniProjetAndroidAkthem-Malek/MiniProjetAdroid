f56873e2149f5560403ea615df96ba95fb0cca32
akthemarfaoui <akthem.arfaoui@esprit.tn>
2019-12-24 15:46:06 +0100

    Amélioration graphique**** #Akthem

--- app/src/main/java/com/example/miniprojectakthemmalek/view/AuthentificationActivity.java
+++ app/src/main/java/com/example/miniprojectakthemmalek/view/AuthentificationActivity.java
@@ -1,74 +1,22 @@
 package com.example.miniprojectakthemmalek.view;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.app.Activity;
-import android.app.Service;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
-import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.IBinder;
 import android.os.StrictMode;
-import android.provider.MediaStore;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.JsonReader;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.miniprojectakthemmalek.R;
 import com.example.miniprojectakthemmalek.model.api.RetrofitInstance;
 import com.example.miniprojectakthemmalek.model.api.entityInterface.IUser;
-import com.example.miniprojectakthemmalek.model.entities.Follow;
-import com.example.miniprojectakthemmalek.model.entities.Invitation;
-import com.example.miniprojectakthemmalek.model.entities.Post;
-import com.example.miniprojectakthemmalek.model.entities.User;
-import com.example.miniprojectakthemmalek.model.repositories.FollowRepository;
-import com.example.miniprojectakthemmalek.model.repositories.GeoLocationRepository;
-import com.example.miniprojectakthemmalek.model.repositories.GroupPostsRepository;
-import com.example.miniprojectakthemmalek.model.repositories.ImageRepository;
-import com.example.miniprojectakthemmalek.model.repositories.InvitationRepository;
-import com.example.miniprojectakthemmalek.model.repositories.PostRepository;
-import com.example.miniprojectakthemmalek.model.repositories.UserRepository;
-import com.example.miniprojectakthemmalek.view.database.AppDatabase;
 import com.example.miniprojectakthemmalek.view.fragments.AccountsFragment;
-import com.example.miniprojectakthemmalek.view.utils.GeoCoder.GeoLocationManager;
-import com.example.miniprojectakthemmalek.view.utils.JavaMailApi.SendMail;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.snackbar.Snackbar;
 import com.example.miniprojectakthemmalek.view.utils.Tools;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.gson.JsonElement;
-
-import org.json.JSONArray;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -83,15 +31,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
 public class AuthentificationActivity extends AppCompatActivity {
